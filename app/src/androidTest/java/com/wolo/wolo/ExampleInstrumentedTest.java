4d2daa9959eaa0dd76e7cc46dac5e5cda748270b
Mike <mnseleznev@mail.ru>
2019-04-13 18:03:12 +0300

    13.04.2019

--- app/src/androidTest/java/com/wolo/wolo/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/wolo/wolo/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.a222", appContext.getPackageName());
+        assertEquals("com.wolo.a222", appContext.getPackageName());
     }
 }
